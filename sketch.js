c665b9905da6c7920652cd75d3033facfc7b1024
eggnoggin0 <37082616+eggnoggin0@users.noreply.github.com>
2018-04-17 16:20:33 -0400

    updated file names, preload files

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
 var state; // 0, 1, or 2
 
-function preload() {
+//images
+var fluteImg;
+var birdWhistleImg;
+var ocarinaImg;
+var hamastsaImg;
+var shellTrumpetImg;
+var panpipeImg;
+var oysterCatcherRattleImg;
+var peyoteRattleImg;
 
+//sounds
+var fluteSound;
+var birdWhistleSound;
+var ocarinaSound;
+var shellTrumpetSound;
+var panpipeSound;
+var peyoteRattleSound;
+
+function preload() {
+	//images
+	fluteImg = loadImage("images/1_flute.png");
+	birdWhistleImg = loadImage("images/2_birdWhistle.png");
+	ocarinaImg = loadImage("images/3_ocarina.png");
+	hamastsaImg = loadImage("images/4_hamastsa.png");
+	shellTrumpetImg = loadImage("images/5_incisedShellTrumpet.png");
+	panpipeImg = loadImage("images/6_panpipe.png");
+	oysterCatcherRattleImg = loadImage("images/7_oysterCatcherRattle.png");
+	peyoteRattleImg = loadImage("images/8_peyoteRattle.png");
+
+	//sounds
+	fluteSound = loadSound("sounds/flute.mp3");
+	birdWhistleSound = loadSound("sounds/birdWhistle.mp3");
+	ocarinaSound = loadSound("sounds/ocarina.mp3");
+	shellTrumpetSound = loadSound("sounds/shellTrumpet.mp3");
+	panpipeSound = loadSound("sounds/panpipe.mp3");
+	peyoteRattleSound = loadSound("sounds/peyoteRattle.mp3");
 }
 
 function setup() {
